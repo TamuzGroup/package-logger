ebb2a90d2401a5cc6aa02a507f3eb20628ca4c84
Alexander Mermelshtein <alexandermermelshtein@alexanders-mbp.com>
2021-07-25 14:48:06 +0300

    making functions private

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 const winston = require('winston');
 require('winston-daily-rotate-file');
 
+function createMessage (fileName, text, level, serviceName, env) {
+  // Format: level|time|platform|Environment|file name|message|
+  const tNowStr = new Date().toISOString();
+  return `${level}|${tNowStr}|${serviceName}|${env}|${fileName}|${text}|`;
+}
+
+function parseParameters (data) {
+  const fullStr = data.reduce((acc, s) => {
+    let val = '';
+    try {
+      if (s.stack) val = `\n${s.stack}\n`; // if it is an exception
+      else if (typeof s === 'object') val = JSON.stringify(s);
+      else if (s) val = s.toString();
+    } catch (e) { val = e.stack; }
+    return `${acc} ${val}`;
+  }, '');
+  // convert all \n to .\t
+  return fullStr.trim().replace(new RegExp('\n', 'g'), () => '.\t');
+}
+
+function getCallDetails() {
+  try {
+    const e = new Error();
+    const frame = e.stack.split('\n')[3];
+    const lineNumber = frame.split(':').reverse()[1];
+    const fileName = frame.split('/').reverse()[0].split(':')[0];
+    return `${fileName}:${lineNumber}`;
+  } catch (e) {
+    console.error(e);
+    return 'FileNameError';
+  }
+}
+
 class BaseLogger {
 
   constructor(serviceName, environmentName) {
@@ -27,72 +60,39 @@ class BaseLogger {
     }
   };
 
-  createMessage (fileName, text, level, serviceName, env) {
-    // Format: level|time|platform|Environment|file name|message|
-    const tNowStr = new Date().toISOString();
-    return `${level}|${tNowStr}|${serviceName}|${env}|${fileName}|${text}|`;
-  };
-
-  parseParameters (data) {
-    const fullStr = data.reduce((acc, s) => {
-      let val = '';
-      try {
-        if (s.stack) val = `\n${s.stack}\n`; // if it is an exception
-        else if (typeof s === 'object') val = JSON.stringify(s);
-        else if (s) val = s.toString();
-      } catch (e) { val = e.stack; }
-      return `${acc} ${val}`;
-    }, '');
-    // convert all \n to .\t
-    return fullStr.trim().replace(new RegExp('\n', 'g'), () => '.\t');
-  };
-
-  getCallDetails() {
-    try {
-      const e = new Error();
-      const frame = e.stack.split('\n')[3];
-      const lineNumber = frame.split(':').reverse()[1];
-      const fileName = frame.split('/').reverse()[0].split(':')[0];
-      return `${fileName}:${lineNumber}`;
-    } catch (e) {
-      console.error(e);
-      return 'FileNameError';
-    }
-  };
-
   error(...data) {
    try {
-     const detailedFile = this.getCallDetails();
-     const fullStr = this.parseParameters(data);
+     const detailedFile = getCallDetails();
+     const fullStr = parseParameters(data);
      console.error(...data);
-     this.logger.error(this.createMessage(detailedFile, fullStr, 'ERROR', this.service, this.environment));
+     this.logger.error(createMessage(detailedFile, fullStr, 'ERROR', this.service, this.environment));
    } catch (e) { console.error(e); }
   };
 
   info(...data) {
      try {
-       const detailedFile = this.getCallDetails();
-       const fullStr = this.parseParameters(data);
+       const detailedFile = getCallDetails();
+       const fullStr = parseParameters(data);
        console.info(...data);
-       this.logger.info(this.createMessage(detailedFile, fullStr, 'INFO', this.service, this.environment));
+       this.logger.info(createMessage(detailedFile, fullStr, 'INFO', this.service, this.environment));
      } catch (e) { console.error(e); }
    };
 
    warn(...data) {
      try {
-       const detailedFile = this.getCallDetails();
-       const fullStr = this.parseParameters(data);
+       const detailedFile = getCallDetails();
+       const fullStr = parseParameters(data);
        console.warn(...data);
-       this.logger.warn(this.createMessage(detailedFile, fullStr, 'WARN', this.service, this.environment));
+       this.logger.warn(createMessage(detailedFile, fullStr, 'WARN', this.service, this.environment));
      } catch (e) { console.error(e); }
    };
 
    debug(...data) {
      try {
-       const detailedFile = this.getCallDetails();
-       const fullStr = this.parseParameters(data);
+       const detailedFile = getCallDetails();
+       const fullStr = parseParameters(data);
        console.debug(...data);
-       this.logger.debug(this.createMessage(detailedFile, fullStr, 'DEBUG', this.service, this.environment));
+       this.logger.debug(createMessage(detailedFile, fullStr, 'DEBUG', this.service, this.environment));
      } catch (e) { console.error(e); }
    };
 }
