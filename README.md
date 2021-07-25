495de8192fa168347bb30897ee94ba7db9b82ba9
Alexander Mermelshtein <alexandermermelshtein@alexanders-mbp.com>
2021-07-25 14:37:00 +0300

    changing to class

--- README.md
+++ README.md
@@ -11,14 +11,56 @@ Logger utility for Givers repos
 3. Each file size is up to: 100MB
 4. Maximum number of files is 3
 
-### Usage
+### Messages Format
+level|time|serviceName|environmentName|fileName:LineNumber|text|
+
+level, time, fileName:LineNumber - automatic fields
+
+text - is the logged argument
+
+serviceName, environmentName - can be configured via dedicated functions
+
+
+### Usage - Simple
 1. Add to package.json dependencies
 
 2. creating the logger in each file where it will be used:
-const logger = require('package-logger')(__filename, processName, environmentName);
+const logger = require('package-logger');
 
 3. Using the logger:
 logger.info('this is an info level log');
 logger.warn('this is a warning level log');
 try { throw new Error('logging an exception'); }
 catch (e) { logger.error('this is an exception', e); }
+
+
+### Usage - Advanced
+1. Add to package.json dependencies
+
+2. creating the myLogger module:
+   const logger = require('package-logger');
+
+   logger.updateServiceName('ServiceName');
+   logger.updateEnvironmentName('EnvironmentName');
+    
+   const {
+    info, error, debug, warn,
+   } = logger.function();
+    
+   module.exports = {
+    info,
+    error,
+    debug,
+    warn,
+   };
+   
+   const logger = require('package-logger');
+
+3. creating the logger in each file where it will be used:
+   const logger = require('myLogger');
+
+4. Using the logger:
+   logger.info('this is an info level log');
+   logger.warn('this is a warning level log');
+   try { throw new Error('logging an exception'); }
+   catch (e) { logger.error('this is an exception', e); }
--- index.js
+++ index.js
@@ -1,98 +1,96 @@
 const winston = require('winston');
 require('winston-daily-rotate-file');
 
-const logger = winston.createLogger({
-  levels: winston.config.npm.levels,
-  format: winston.format.simple(),
-  transports: [
-    new winston.transports.Console({ format: winston.format.simple() }),
-    new winston.transports.DailyRotateFile({
-      filename: '/tmp/logs-%DATE%.log',
-      datePattern: 'YYYY-MM-DD-HH',
-      zippedArchive: true,
-      maxSize: '100m', // 100MB
-      maxFiles: '3', // delete older files, keep up to last 3
-    }),
-  ],
-});
+class BaseLogger {
 
-let service = 'SERVICE';
-let environment = 'ENV';
+  constructor(serviceName, environmentName) {
+    this.service = serviceName;
+    this.environment = environmentName;
 
-const updateServiceName = (serviceName) => { service = serviceName; }
+    this.logger = winston.createLogger({
+      levels: winston.config.npm.levels,
+      format: winston.format.simple(),
+      transports: [
+        new winston.transports.Console({ format: winston.format.simple() }),
+        new winston.transports.DailyRotateFile({
+          filename: '/tmp/logs-%DATE%.log',
+          datePattern: 'YYYY-MM-DD-HH',
+          zippedArchive: true,
+          maxSize: '100m', // 100MB
+          maxFiles: '3', // delete older files, keep up to last 3
+        }),
+      ],
+    });
+  };
 
-const updateEnvironmentName = (environmentName) => { environment = environmentName; }
-
-const createMessage = (fileName, text, level, serviceName, env) => {
-  // Format: level|time|platform|Environment|file name|message|
-  const tNowStr = new Date().toISOString();
-  return `${level}|${tNowStr}|${serviceName}|${env}|${fileName}|${text}|`;
+  createMessage (fileName, text, level, serviceName, env) {
+    // Format: level|time|platform|Environment|file name|message|
+    const tNowStr = new Date().toISOString();
+    return `${level}|${tNowStr}|${serviceName}|${env}|${fileName}|${text}|`;
 };
 
-const parseParameters = function (data) {
-  const fullStr = data.reduce((acc, s) => {
-    let val = '';
+  parseParameters (data) {
+    const fullStr = data.reduce((acc, s) => {
+      let val = '';
+      try {
+        if (s.stack) val = `\n${s.stack}\n`; // if it is an exception
+        else if (typeof s === 'object') val = JSON.stringify(s);
+        else if (s) val = s.toString();
+      } catch (e) { val = e.stack; }
+      return `${acc} ${val}`;
+    }, '');
+    // convert all \n to .\t
+    return fullStr.trim().replace(new RegExp('\n', 'g'), () => '.\t');
+};
+
+  getCallDetails() {
     try {
-      if (s.stack) val = `\n${s.stack}\n`; // if it is an exception
-      else if (typeof s === 'object') val = JSON.stringify(s);
-      else if (s) val = s.toString();
-    } catch (e) { val = e.stack; }
-    return `${acc} ${val}`;
-  }, '');
-  // convert all \n to .\t
-  return fullStr.trim().replace(new RegExp('\n', 'g'), () => '.\t');
-};
+      const e = new Error();
+      const frame = e.stack.split('\n')[3];
+      const lineNumber = frame.split(':').reverse()[1];
+      const fileName = frame.split('/').reverse()[0].split(':')[0];
+      return `${fileName}:${lineNumber}`;
+    } catch (e) {
+      console.log(e);
+      return 'FileNameError';
+    }
+  };
 
-const getCallDetails = () => {
-  try {
-    const e = new Error();
-    const frame = e.stack.split('\n')[3];
-    const lineNumber = frame.split(':').reverse()[1];
-    const fileName = frame.split('/').reverse()[0].split(':')[0];
-    return `${fileName}:${lineNumber}`;
-  } catch (e) {
-    console.log(e);
-    return 'FileNameError';
-  }
-};
+  error(...data) {
+   try {
+     const detailedFile = this.getCallDetails();
+     const fullStr = this.parseParameters(data);
+     console.error(...data);
+     this.logger.error(this.createMessage(detailedFile, fullStr, 'ERROR', this.service, this.environment));
+   } catch (e) { console.error(e); }
+  };
 
-module.exports = {
-  function () {
-    return {
-      error(...data) {
-        try {
-          const detailedFile = getCallDetails();
-          const fullStr = parseParameters(data);
-          console.error(...data);
-          logger.error(createMessage(detailedFile, fullStr, 'ERROR', service, environment));
-        } catch (e) { console.error(e); }
-      },
-      info(...data) {
-        try {
-          const detailedFile = getCallDetails();
-          const fullStr = parseParameters(data);
-          console.info(...data);
-          logger.info(createMessage(detailedFile, fullStr, 'INFO', service, environment));
-        } catch (e) { console.error(e); }
-      },
-      warn(...data) {
-        try {
-          const detailedFile = getCallDetails();
-          const fullStr = parseParameters(data);
-          console.warn(...data);
-          logger.warn(createMessage(detailedFile, fullStr, 'WARN', service, environment));
-        } catch (e) { console.error(e); }
-      },
-      debug(...data) {
-        try {
-          const detailedFile = getCallDetails();
-          const fullStr = parseParameters(data);
-          console.debug(...data);
-          logger.debug(createMessage(detailedFile, fullStr, 'DEBUG', service, environment));
-        } catch (e) { console.error(e); }
-      },
-    };
-  },
-  updateServiceName,
-  updateEnvironmentName,
-};
+  info(...data) {
+     try {
+       const detailedFile = this.getCallDetails();
+       const fullStr = this.parseParameters(data);
+       console.info(...data);
+       this.logger.info(this.createMessage(detailedFile, fullStr, 'INFO', this.service, this.environment));
+     } catch (e) { console.error(e); }
+   };
+
+   warn(...data) {
+     try {
+       const detailedFile = this.getCallDetails();
+       const fullStr = this.parseParameters(data);
+       console.warn(...data);
+       this.logger.warn(this.createMessage(detailedFile, fullStr, 'WARN', this.service, this.environment));
+     } catch (e) { console.error(e); }
+   };
+
+   debug(...data) {
+     try {
+       const detailedFile = this.getCallDetails();
+       const fullStr = this.parseParameters(data);
+       console.debug(...data);
+       this.logger.debug(this.createMessage(detailedFile, fullStr, 'DEBUG', this.service, this.environment));
+     } catch (e) { console.error(e); }
+   };
+}
+
+module.exports = BaseLogger;
